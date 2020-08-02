332c6c5b9f8a4e6e3e2db57f9208c8f9bd838e72
ChaoticBored <46114415+XenoQueen@users.noreply.github.com>
2020-08-02 12:54:35 -0700

    Defined run method in CLI class

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,5 +1,15 @@
 class CLI
 
-  
+  def run
+    system("clear")
+    @user_input = nil
+    @api = APIService.new
+    @page = 1
+    welcome
+    until @user_input == "2"
+      main_menu
+    end
+    puts "Goodbye!"
+  end
 
 end
