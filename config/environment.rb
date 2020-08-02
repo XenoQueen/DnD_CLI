88e8f333f2f2cd2742912a05cdbb328a69a9ce2a
ChaoticBored <46114415+XenoQueen@users.noreply.github.com>
2020-08-02 11:41:14 -0700

    Added required elements text to environment file

---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+require "pry"
+require "colorize"
+require "uri"
+require "net/http"
+require "json"
+
+require_relative "..lib/models/Classes.rb"
+require_relative "..lib/models/Races.rb"
+require_relative "..lib/models/Subclasses.rb"
+require_relative "..lib/models/Subraces.rb"
+require_relative "../lib/services/APIService.rb"
+require_relative "../lib/CLI.rb"
