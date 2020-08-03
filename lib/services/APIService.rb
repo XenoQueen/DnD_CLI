d54e1f3457d052998551b72a9bc400ffbcecc4d7
ChaoticBored <46114415+XenoQueen@users.noreply.github.com>
2020-08-03 14:37:07 -0700

    Defines fetch_class_by_name method in APIService class

--- lib/services/APIService.rb
+++ lib/services/APIService.rb
@@ -2,4 +2,15 @@ class APIService
 
   BASE_URI = "https://www.dnd5eapi.co/"
 
+  def fetch_class_by_name(name)
+    uri = URI(BASE_URI + "?name=#{name}")
+    classes = make_request(uri)
+    if classes[0]
+      Classes.new(classes[0])
+    else
+      "Could not find a class with that name.".colorize(:red)
+    end
+  end
+
+  
 end
