69803888559a44be2ea44228f826ad11a4791310
ChaoticBored <46114415+XenoQueen@users.noreply.github.com>
2020-08-03 14:27:23 -0700

    Defines initialize and self.all methods in Classes class

--- lib/models/Classes.rb
+++ lib/models/Classes.rb
@@ -1,5 +1,20 @@
 class Classes
 
-  
+  attr_reader :name, :hit_die, :proficiencies, :saving_throws
 
+  @@all = []
+
+  def initialize(class_data)
+    @name = class_data["name"]
+    @hit_die = class_data["hit_die"]
+    @proficiencies = class_data["proficiencies"]
+    @saving_throws = class_data["saving_throws"]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  
 end
