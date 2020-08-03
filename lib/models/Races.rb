e86a6d46dffee078fb6155762c4a04b080599be3
ChaoticBored <46114415+XenoQueen@users.noreply.github.com>
2020-08-03 13:48:52 -0700

    defines initialize and self.all methods for Race class

--- lib/models/Races.rb
+++ lib/models/Races.rb
@@ -1,5 +1,20 @@
 class Races
 
-  
+  attr_reader :name, :size, :speed, :languages
 
+  @@all = []
+
+  def initialize(race_data)
+    @name = race_data["name"]
+    @size = race_data["size"]
+    @speed = race_data["speed"]
+    @languages = race_data["languages"]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  
 end
