813274140b2b30eff49d7196cbe8dcadc960f519
ChaoticBored <46114415+XenoQueen@users.noreply.github.com>
2020-08-04 10:08:53 -0700

    Aesthetic improvements for environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,11 @@
+# Gems
 require "pry"
 require "colorize"
 require "uri"
 require "net/http"
 require "json"
+
+# Classes
 
 require_relative "..lib/models/Classes.rb"
 require_relative "..lib/models/Races.rb"
